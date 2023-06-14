a033ae93f765ac8560ac2eb9e2fe1cd7f4d101e1
Kartik Bhat <76040124+kartik-cyber@users.noreply.github.com>
2023-06-14 18:44:15 +0530

    Simple-calculator(switch-case).java

--- Simple-calculator(switch-case).java
+++ Simple-calculator(switch-case).java
@@ -1,3 +1,4 @@
+//This version of simple calculator is written using switch case in the Java programming, here switch case is used in the operator 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
@@ -7,8 +8,7 @@ public static void main(String args[])
 {
 try
 {
-BufferedReader br = new BufferedReader(new
-InputStreamReader(System.in));
+BufferedReader br = new BufferedReader(newInputStreamReader(System.in));
 System.out.println("Enter first operand");
 int a = Integer.parseInt(br.readLine());
 System.out.println("Enter second operand");
@@ -26,11 +26,9 @@ case "*": res = a * b;
 break;
 case "/": res = a / b;
 break;
-default: throw new java.lang.Error("operator not
-recognized");
+default: throw new java.lang.Error("operator not recognized");
 }
-System.out.println("The result of "+a+" "+op+" "+b+" is:
-"+res);
+System.out.println("The result of "+a+" "+op+" "+b+" is: "+res);
 } //end of try block
 catch(IOException e)
 {
